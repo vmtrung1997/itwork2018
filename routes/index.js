2ff8a74cd72199f15a09c4dd62ea21998fde23e3
unknown <ludinhquan@gmail.com>
2018-05-09 14:51:08 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,29 @@ var mongoose    = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/itwork2018');
 
 /* GET home page. */
-router.get('/:jobId', function(req, res, next) {
+router.get('/jobs', function(req, res, next) {
+    Job.find()
+    .limit(10)
+    .exec()
+    .then(jobs =>{
+        res.render('home/index',{ jobs : jobs});
+    })
+    .catch(err=>{
+        res.status(500).json({err: err})
+    })
+});
+router.get('/company', function(req, res, next) {
+    Company.find()
+    .limit(10)
+    .exec()
+    .then(company =>{
+        res.render('company',{ company : company});
+    })
+    .catch(err=>{
+        res.status(500).json({err: err})
+    })
+});
+router.get('/jobs/:jobId', function(req, res, next) {
     const id = req.params.jobId;
     Job.findById(id)
     .exec()
@@ -22,7 +44,7 @@ router.get('/', function(req, res, next) {
     Job.find()
     .limit(10)
     .then(function(jobs) {
-            res.render('home/index',{jobs : jobs});
+        res.render('home/index',{jobs : jobs});
     });
 });
 module.exports = router;
--- routes/job.js
+++ routes/job.js
@@ -6,7 +6,20 @@ var mongoose    = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/itwork2018');
 
 /* GET home page. */
-router.get('/:jobId', function(req, res, next) {
+
+router.get('/jobs', function(req, res, next) {
+    Job.find()
+    .limit(10)
+    .exec()
+    .then(jobs =>{
+        res.render('index',{ jobs : jobs});
+    })
+    .catch(err=>{
+        res.status(500).json({err: err})
+    })
+});
+
+router.get('/jobs/:jobId', function(req, res, next) {
     const id = req.params.jobId;
     Job.findById(id)
     .exec()
@@ -17,7 +30,6 @@ router.get('/:jobId', function(req, res, next) {
         res.status(500).json({err: err})
     })
 });
-
 router.get('/', function(req, res, next) {
     Job.find().limit(10).then(function(jobs) {
         var data = []
